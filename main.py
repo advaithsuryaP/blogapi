a7c6e737382b50902c4d46023fdf566bde4c3f45
advaithsuryaP <padvaithsurya@gmail.com>
2026-07-09 19:26:32 -0400

    Add API endpoint to retrieve posts in main.py
    - Introduced a new GET route '/api/posts' that returns a list of sample posts with author, content, and timestamps.
    - Each post is represented as a dictionary within a list.

--- main.py
+++ main.py
@@ -5,3 +5,23 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "Hello World!"}
+
+@app.get("/api/posts")
+def get_posts():
+    posts: list[dict] = [
+        {
+            "id": 1,
+            "author": "Advaith",
+            "content": "This is a test post",
+            "created_at": "2026-01-01",
+            "updated_at": "2026-01-01"
+        },
+        {
+            "id": 2,
+            "author": "John Doe",
+            "content": "This is a test post 2",
+            "created_at": "2026-01-02",
+            "updated_at": "2026-01-02"
+        }        
+    ]
+    return posts
