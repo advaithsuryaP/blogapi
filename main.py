d737028ae327cabe8840a15f500ba3db45726f62
advaithsuryaP <padvaithsurya@gmail.com>
2026-07-10 19:09:45 -0400

    Enhance home page to display posts dynamically
    - Updated the home route to pass a list of posts to the Jinja2 template for rendering.
    - Modified 'home.html' to iterate over posts and display the author and content for each post.

--- main.py
+++ main.py
@@ -4,26 +4,28 @@ from fastapi.templating import Jinja2Templates
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
+posts: list[dict] = [
+    {
+        "id": 1,
+        "author": "Advaith",
+        "content": "This is a test post",
+        "created_at": "2026-01-01",
+        "updated_at": "2026-01-01"
+    },
+    {
+        "id": 2,
+        "author": "John Doe",
+        "content": "This is a test post 2",
+        "created_at": "2026-01-02",
+        "updated_at": "2026-01-02"
+    }        
+]
+
+
 @app.get("/", include_in_schema=False)
 def home(request: Request):
-    return templates.TemplateResponse(request, "home.html")
+    return templates.TemplateResponse(request, "home.html", {"posts": posts})
 
 @app.get("/api/posts")
 def get_posts():
-    posts: list[dict] = [
-        {
-            "id": 1,
-            "author": "Advaith",
-            "content": "This is a test post",
-            "created_at": "2026-01-01",
-            "updated_at": "2026-01-01"
-        },
-        {
-            "id": 2,
-            "author": "John Doe",
-            "content": "This is a test post 2",
-            "created_at": "2026-01-02",
-            "updated_at": "2026-01-02"
-        }        
-    ]
     return posts
