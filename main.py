c262c812cc4e6bb61aba3c0581747e181edf59f1
advaithsuryaP <padvaithsurya@gmail.com>
2026-07-18 05:03:03 -0400

    Update post data structure and API response model
    - Added titles to existing posts for better identification.
    - Replaced 'created_at' and 'updated_at' fields with 'date_posted' for clarity.
    - Updated the response model for the post creation endpoint to 'PostResponse' for consistency with the new data structure.

--- main.py
+++ main.py
@@ -11,17 +11,17 @@ templates = Jinja2Templates(directory="templates")
 posts: list[dict] = [
     {
         "id": 1,
+        "title": "First Post",
         "author": "Advaith",
         "content": "This is a test post",
-        "created_at": "2026-01-01",
-        "updated_at": "2026-01-01"
+        "date_posted": "Aptil 2nd 2026"
     },
     {
         "id": 2,
+        "title": "Second Post",
         "author": "John Doe",
         "content": "This is a test post 2",
-        "created_at": "2026-01-02",
-        "updated_at": "2026-01-02"
+        "date_posted": "May 3nd 2026"
     }        
 ]
 
@@ -41,7 +41,7 @@ def get_post(post_id: int):
             return post
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
 
-@app.post("/api/posts", response_model = PostCreate, status_code=status.HTTP_201_CREATED)
+@app.post("/api/posts", response_model = PostResponse, status_code=status.HTTP_201_CREATED)
 def create_post(post: PostCreate):
     new_id = max(p["id"] for p in posts) + 1 if posts else 1
     new_post = {
